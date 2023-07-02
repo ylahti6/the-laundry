ebf871283e0ef8799d15e4f33ae133025fb53877
yannick <himynameisyannick@gmail.com>
2023-07-03 00:48:57 +0100

    sample page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,64 @@
 import React from "react";
 import styled from "styled-components";
 
+import "../styles/Global/GlobalStyle.css";
+import "../Styles/Variable/Variables.css";
+
 export default function Hero() {
-  const mainPage = styled.div`
-    h1 { text-transform: capitalize}
+  const MainPage = styled.div`
+    font-family: var(--font-sans);
+
+    .container {
+      /* background: red; */
+      position: relative;
+      height: 100vh;
+      width: 100%;
+
+      h1,
+      h3 {
+        text-transform: uppercase;
+        font-weight: 600;
+        margin: 1rem;
+      }
+      
+      .top {
+        display: flex;
+      }
+
+      .bot {
+        position: absolute;
+        bottom: 0;
+        h3 { display: flex; }
+        h1 { font-size: 10vw; }
+        span { display: flex; }
+      }
+      
+      
+    }
   `;
 
   return (
-    <mainPage>
+    <MainPage>
       <div className="container">
-        <div className="top">
-          <h1>what we do,</h1>
-          <h1>about,</h1> 
-          <h1>vision,</h1> 
-          <h1>e5 9bu</h1>
-       </div>
 
-      <div className="bottom">
-       <h1>the laundry</h1>
-       <h3>contact</h3>
-       <h3>journal</h3>
-       <h3>Coded by : Yannick Lahti</h3>
+        <div className="top">
+          <h3>what we do,</h3>
+          <h3>about,</h3>
+          <h3>vision,</h3>
+          <h3>e5 9bu</h3>
+        </div>
+
+        <div className="bot">
+          
+        <h1>the laundry</h1>
+        <span>
+          <h3>contact</h3>
+          <h3>journal</h3>
+          <h3>Yannick Lahti</h3>
+          </span>
+        </div>
+
       </div>
-      </div>
-    </mainPage>
+    </MainPage>
   );
 }
